e7137f9604c1f371aa9979d47d04fdf4ebb7998d
China131 <13162255392@163.com>
2016-07-18 15:55:45 +0800

    Initial Commit

---
+++ QQDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQDemo
+//
+//  Created by 简豪 on 16/7/18.
+//  Copyright © 2016年 codingMan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
